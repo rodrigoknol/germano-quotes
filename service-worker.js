e0dd9a72bd8d566d4b7bca82920393306f0e56c2
Rodrigo Thaler Knolseisen <rtknolseisen@gmail.com>
2021-12-04 14:25:23 -0300

    finishes service worker

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const version = "2.0";
+const version = "1.0";
 const cacheName = `germano-quotes-v${version}`;
 
 const main = [
+  "/index.html",
   "/",
   "/css/style.css",
   "/css/reset.css",
   "/img/favicon/favicon.ico",
   "/img/favicon/favicon-32x32.png",
-  "/manifest-webmanifest",
 ];
 
 const static = [
